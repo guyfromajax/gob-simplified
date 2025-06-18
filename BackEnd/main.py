891a4461be79ecaf1d99d93fbc50898510bf8a8c
jamesdavies <jamiejosephdavies@gmail.com>
2025-06-18 13:15:27 -0400

    updated players as Mongo objects 5

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -3,10 +3,13 @@ import json
 # from BackEnd.api import app
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
     raise RuntimeError("❌ MONGO_URI is not set in the environment.")
+print(f"✅ Using MONGO_URI: {MONGO_URI}")
 
 client = MongoClient(MONGO_URI)
 db = client["GOB"]
