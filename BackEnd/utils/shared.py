58a407241098a1f91e6f652f85b5b856db6a7fd9
jamesdavies <jamiejosephdavies@gmail.com>
2025-07-01 09:57:25 -0400

    updated drawPlayer

--- BackEnd/utils/shared.py
+++ BackEnd/utils/shared.py
@@ -332,6 +332,16 @@ def unpack_game_context(game):
     )
 
 def summarize_game_state(game):
+    home_colors = {
+        "primary_color": game.home_team.attributes.get("primary_color", "#000000"),
+        "secondary_color": game.home_team.attributes.get("secondary_color", "#ffffff")
+    }
+    away_colors = {
+        "primary_color": game.away_team.attributes.get("primary_color", "#000000"),
+        "secondary_color": game.away_team.attributes.get("secondary_color", "#ffffff")
+    }
+
+    
     players = []
     for team_key, team_obj in [("home", game.home_team), ("away", game.away_team)]:
         for pos, player in team_obj.lineup.items():
@@ -360,6 +370,8 @@ def summarize_game_state(game):
         "turns": game.turns,
         "home_team_name": game.home_team.name,
         "away_team_name": game.away_team.name,
+        "home_team_colors": home_colors,
+        "away_team_colors": away_colors,
         "score": game.score,
         "home_team_id": game.home_team.team_id,
         "players": players
