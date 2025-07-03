18ea6114a415d52b5bbcca74b2503066f9076d43
jamesdavies <jamiejosephdavies@gmail.com>
2025-07-03 13:43:07 -0400

    multi step animation 19

--- BackEnd/playcall_skeletons/inside_skeletons.py
+++ BackEnd/playcall_skeletons/inside_skeletons.py
@@ -1,28 +1,28 @@
 from BackEnd.constants import HCO_STRING_SPOTS, ACTIONS
 
-HCO_STRING_SPOTS = {
-    "key": {"x": 64, "y": 25},
-    "upper midWing": {"x": 68, "y": 36}, 
-    "lower midWing": {"x": 68, "y": 14},
-    "upper wing": {"x": 73, "y": 40}, 
-    "lower wing": {"x": 73, "y": 10},
-    "upper midCorner": {"x": 81, "y": 43}, 
-    "lower midCorner": {"x": 81, "y": 7},
-    "upper corner": {"x": 88, "y": 44}, 
-    "lower corner": {"x": 88, "y": 6},
-    "upper highPost": {"x": 74, "y": 32}, 
-    "lower highPost": {"x": 74, "y": 19},
-    "upper midPost": {"x": 80, "y": 32}, 
-    "lower midPost": {"x": 80, "y": 19},
-    "upper lowPost": {"x": 86, "y": 32}, 
-    "lower lowPost": {"x": 86, "y": 19}, 
-    "topLane": {"x": 74, "y": 25},
-    "midLane": {"x": 80, "y": 25}, 
-    "upper apex": {"x": 80, "y": 36}, 
-    "lower apex": {"x": 80, "y": 15},
-    "upper midBaseline": {"x": 89, "y": 36}, 
-    "lower midBaseline": {"x": 89, "y": 15},
-}
+# HCO_STRING_SPOTS = {
+#     "key": {"x": 64, "y": 25},
+#     "upper midWing": {"x": 68, "y": 36}, 
+#     "lower midWing": {"x": 68, "y": 14},
+#     "upper wing": {"x": 73, "y": 40}, 
+#     "lower wing": {"x": 73, "y": 10},
+#     "upper midCorner": {"x": 81, "y": 43}, 
+#     "lower midCorner": {"x": 81, "y": 7},
+#     "upper corner": {"x": 88, "y": 44}, 
+#     "lower corner": {"x": 88, "y": 6},
+#     "upper highPost": {"x": 74, "y": 32}, 
+#     "lower highPost": {"x": 74, "y": 19},
+#     "upper midPost": {"x": 80, "y": 32}, 
+#     "lower midPost": {"x": 80, "y": 19},
+#     "upper lowPost": {"x": 86, "y": 32}, 
+#     "lower lowPost": {"x": 86, "y": 19}, 
+#     "topLane": {"x": 74, "y": 25},
+#     "midLane": {"x": 80, "y": 25}, 
+#     "upper apex": {"x": 80, "y": 36}, 
+#     "lower apex": {"x": 80, "y": 15},
+#     "upper midBaseline": {"x": 89, "y": 36}, 
+#     "lower midBaseline": {"x": 89, "y": 15},
+# }
 
 
 INSIDE_SCENE_1 = {
