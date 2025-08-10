6267f61e0714743cc93e3f4dd74c73c3f8bd7558
jamesdavies <jamiejosephdavies@gmail.com>
2025-08-10 11:03:24 -0400

    printing team scores after each turn

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -243,6 +243,7 @@ def run_simulation(home_team_name, away_team_name):
 
     while gm.game_state["time_remaining"] > 0:
         gm.simulate_macro_turn()
+        print(f"🏀 {gm.home_team.name}: {gm.game_state['score'][gm.home_team.name]} | {gm.away_team.name}: {gm.game_state['score'][gm.away_team.name]}")
 
     # Print all game statistics including defense score stats
     # gm.print_game_statistics()
