e247c05bff4681385a9e49588c46c4d044ef4059
jamesdavies <jamiejosephdavies@gmail.com>
2025-10-02 16:21:53 -0400

    added skeleton to turn return data

--- BackEnd/constants.py
+++ BackEnd/constants.py
@@ -112,6 +112,11 @@ HCO_STRING_SPOTS = {
     "lower apex": {"x": 80, "y": 15},
     "upper midBaseline": {"x": 89, "y": 36}, 
     "lower midBaseline": {"x": 89, "y": 15},
+    "deep key": {"x": 57, "y": 25},
+    "deep lower wing": {"x": 57, "y": 15},
+    "deep lower baseline": {"x": 57, "y": 5},
+    "deep upper wing": {"x": 57, "y": 35},
+    "deep upper baseline": {"x": 57, "y": 45}
 }
 
 # Shared court coordinates
