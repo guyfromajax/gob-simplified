b3842911895652d0284b94ec000948c304bc44d1
jamesdavies <jamiejosephdavies@gmail.com>
2025-10-02 16:53:41 -0400

    set fcp & hct to 4

--- BackEnd/models/team_manager.py
+++ BackEnd/models/team_manager.py
@@ -57,8 +57,8 @@ class TeamManager:
             "tempo": random.randint(0, 4),
             "aggression": random.randint(0, 4),
             "fast_break": random.randint(0, 4),
-            "half_court_trap": 0,
-            "full_court_press": 0,
+            "half_court_trap": 4,
+            "full_court_press": 4,
         }
 
     def _init_playcall_settings(self):
