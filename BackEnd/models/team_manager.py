c0200d0d1bf11d79c32ecb7b23a81081905d936c
jamesdavies <jamiejosephdavies@gmail.com>
2025-10-07 16:31:48 -0400

    hc trap set to 0

--- BackEnd/models/team_manager.py
+++ BackEnd/models/team_manager.py
@@ -57,7 +57,7 @@ class TeamManager:
             "tempo": random.randint(0, 4),
             "aggression": random.randint(0, 4),
             "fast_break": random.randint(0, 4),
-            "half_court_trap": 4,
+            "half_court_trap": 0,
             "full_court_press": 4,
         }
 
