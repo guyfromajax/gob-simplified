e4a6b1c1eeb30f4a1d8e00902e52fc0efcf1179e
jamesdavies <jamiejosephdavies@gmail.com>
2025-10-14 20:08:53 -0400

    defensive placement tweak

--- BackEnd/utils/shared_defense.py
+++ BackEnd/utils/shared_defense.py
@@ -95,7 +95,7 @@ def assign_non_bh_defender_coords(o_coords, ball_coords, aggression_level, is_aw
 
         # x = ox + int(delta_x * 0.3) + (x_direction * d_spacing)
         # y = oy + int(delta_y * 0.3) + (y_direction * d_spacing)
-        x = ox + int(delta_x * 0.3)
-        y = oy + int(delta_y * 0.3)
+        x = ox + (int(delta_x * 0.3) * x_direction)
+        y = oy + (int(delta_y * 0.3) * y_direction)
 
         return {"x": x, "y": y}
