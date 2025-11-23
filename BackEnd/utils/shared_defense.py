6b0e53b5815c8ef88af049362bd92b8d4bfcea74
jamesdavies <jamiejosephdavies@gmail.com>
2025-11-22 19:29:05 -0500

    Fix BH defender x_direction: use -1 for away offense (LEFT), +1 for home offense (RIGHT)

--- BackEnd/utils/shared_defense.py
+++ BackEnd/utils/shared_defense.py
@@ -28,17 +28,16 @@ def assign_bh_defender_coords(ball_coords, aggression_level: str, is_away_offens
     x_bh = ball_coords["x"]
     y_bh = ball_coords["y"]
 
-    # SIMPLE RULE: Defender positioning based on which basket is being defended
-    # - When HOME team has ball: they attack AWAY basket (x=10), defender defends AWAY basket, so defender to LEFT (x_direction = -1)
-    # - When AWAY team has ball: they attack AWAY basket (x=10), defender defends AWAY basket, so defender to LEFT (x_direction = -1)
+    # SIMPLE RULE: Defender positioning relative to ball handler
+    # - When AWAY team has ball: defender should be to LEFT of ball handler (x less) = x_direction = -1
+    # - When HOME team has ball: defender should be to RIGHT of ball handler (x more) = x_direction = +1
     # This positions the defender between the ball handler and the basket they're defending
-    # NOTE: In home orientation, the away basket is always at x=10, so defender is always to the LEFT
     if is_away_offense:
-        # Away team has ball - they attack AWAY basket (x=10), defender defends AWAY basket, so defender to LEFT
+        # Away team has ball - defender to LEFT of ball handler (x less)
         x_direction = -1
     else:
-        # Home team has ball - they attack AWAY basket (x=10), defender defends AWAY basket, so defender to LEFT
-        x_direction = -1
+        # Home team has ball - defender to RIGHT of ball handler (x more)
+        x_direction = 1
     
     # Determine which basket is being attacked (for verification/debugging)
     target_basket = AWAY_RIM_COORDS if not is_away_offense else HOME_RIM_COORDS
