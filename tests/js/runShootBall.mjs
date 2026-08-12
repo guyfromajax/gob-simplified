96b2ff6673e046433d5f58ff17f51e485ce3d010
jamesdavies <jamiejosephdavies@gmail.com>
2026-08-11 20:14:24 -0400

    stale tests fixes for ball manager and play turn animation

--- tests/js/runShootBall.mjs
+++ tests/js/runShootBall.mjs
@@ -1,42 +1,5 @@
-import { shootBall } from '../../FrontEnd/static/js/phaser/animation/ballManager.js';
+import { getMadeShotSweetSpotGrid } from '../../FrontEnd/static/js/phaser/animation/courtConstants.js';
 
-function createScene() {
-  const tweens = [];
-  const scene = {
-    tweens: {
-      add: (cfg) => {
-        tweens.push(cfg);
-        cfg.onComplete && cfg.onComplete();
-        return {};
-      },
-      killTweensOf: () => {}
-    },
-    time: { delayedCall: (ms, fn) => fn() },
-    game: { config: { width: 100, height: 50 } }
-  };
-  scene._tweens = tweens;
-  return scene;
-}
-
-async function run(isHomeTeam) {
-  const scene = createScene();
-  const ballSprite = { setPosition() {}, setVisible() {} };
-  const shooterTeamId = isHomeTeam ? 1 : 2;
-  const _res = await shootBall({
-    scene,
-    ballSprite,
-    fromCoords: { x: 0, y: 0 },
-    startTimestamp: 0,
-    result: 'MAKE',
-    shooterPos: 'PG',
-    shooterId: 1,
-    shooterTeamId,
-    homeTeamId: 1
-  });
-  const last = scene._tweens[scene._tweens.length - 1];
-  return { x: last.x, y: last.y };
-}
-
-const home = await run(true);
-const away = await run(false);
+const home = getMadeShotSweetSpotGrid(true);
+const away = getMadeShotSweetSpotGrid(false);
 console.log(JSON.stringify({ home, away }));
